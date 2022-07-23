01f6a0d9cdfb440d542f7639178ad23f84b1f4a7
Тимофей Чижевский <chizhevskii.tima@gmail.com>
2022-07-23 21:41:25 +0200

    dbContext

--- Movie/Context/MovieContext.cs
+++ Movie/Context/MovieContext.cs
@@ -6,6 +6,7 @@ namespace Movie.Context
     public class MovieContext : DbContext
     {
         public DbSet<Film> Films { get; set; }
+        public DbSet<Serial> Serials { get; set; }
         public DbSet<Admin> Admins { get; set; }
         public MovieContext(DbContextOptions<MovieContext> dbContext)
             : base(dbContext)
@@ -26,7 +27,7 @@ namespace Movie.Context
             modelBuilder.Entity<Serial>().Property(x => x.Name).IsRequired().HasColumnType("varchar").HasMaxLength(100);
             modelBuilder.Entity<Serial>().Property(x => x.Season).IsRequired().HasColumnType("int");
             modelBuilder.Entity<Serial>().Property(x => x.Episode).IsRequired().HasColumnType("int");
-            modelBuilder.Entity<Serial>().Property(x => x.Completed).IsRequired().HasColumnType("bool").HasDefaultValue(false);
+            modelBuilder.Entity<Serial>().Property(x => x.Completed).IsRequired().HasColumnType("bit").HasDefaultValue(false);//bool
             modelBuilder.Entity<Serial>().Property(x => x.Description).IsRequired().HasColumnType("varchar").HasMaxLength(800);
             modelBuilder.Entity<Serial>().Property(x => x.Evaluation).IsRequired().HasColumnType("float").HasMaxLength(1);
             modelBuilder.Entity<Serial>().Property(x => x.PosterPath).IsRequired().HasColumnType("varchar").HasMaxLength(300);
