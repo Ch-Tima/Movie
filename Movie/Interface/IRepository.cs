ff06ff8d42465c33686f0966a45139d0206f99c1
Тимофей Чижевский <chizhevskii.tima@gmail.com>
2022-07-26 13:28:27 +0200

    IRepository

---
+++ Movie/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Movie.Interface
+{
+    public interface IRepository<T>
+    {
+        void Add(T obj);
+        
+        void Update(int id, T newObj);
+        
+        void Delete(T obj);
+        void Delete(int id);
+        
+        List<T> GetAll();
+        T GetId(int id);
+
+    }
+}
