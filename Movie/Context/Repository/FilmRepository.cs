2504e6d0db246b32082185c8932c0847f695177d
Тимофей Чижевский <chizhevskii.tima@gmail.com>
2022-07-26 13:28:13 +0200

    Repository

---
+++ Movie/Context/Repository/FilmRepository.cs
@@ -0,0 +1,48 @@
+﻿using Movie.Interface;
+using Movie.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.Context.Repository
+{
+    public class FilmRepository : IRepository<Film>
+    {
+        private readonly MovieContext _movieContext;
+        public FilmRepository(MovieContext mc) => _movieContext = mc;
+
+        public void Add(Film obj)
+        {
+            _movieContext.Films.Add(obj);
+            _movieContext.SaveChanges();
+        }
+
+        public void Delete(Film obj)
+        {
+            _movieContext.Films.Remove(obj);
+            _movieContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            _movieContext.Films.Remove(_movieContext.Films.First(x => x.Id == id));
+            _movieContext.SaveChanges();
+        }
+
+        public List<Film> GetAll() => _movieContext.Films.ToList();
+
+        public Film GetId(int id) => _movieContext.Films.First(x => x.Id == id);
+
+        public void Update(int id, Film newObj)
+        {
+            var tf = _movieContext.Films.First(x => x.Id == id);
+
+            tf.Name = newObj.Name;
+            tf.Evaluation = newObj.Evaluation;
+            tf.Description = newObj.Description;
+            tf.PosterPath = newObj.PosterPath;
+
+            _movieContext.SaveChanges();
+        }
+    }
+}
---
+++ Movie/Context/Repository/SerialRepository.cs
@@ -0,0 +1,53 @@
+﻿using Movie.Interface;
+using Movie.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.Context.Repository
+{
+    public class SerialRepository : IRepository<Serial>
+    {
+        private readonly MovieContext _movieContext;
+        public SerialRepository(MovieContext mc) => _movieContext = mc;
+
+        public void Add(Serial obj)
+        {
+            _movieContext.Serials.Add(obj);
+            _movieContext.SaveChanges();
+        }
+
+        public void Delete(Serial obj)
+        {
+            _movieContext.Serials.Remove(obj);
+            _movieContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            _movieContext.Serials.Remove(_movieContext.Serials.First(x => x.Id == id));
+            _movieContext.SaveChanges();
+        }
+
+        public List<Serial> GetAll() => _movieContext.Serials.ToList();
+
+        public Serial GetId(int id) => _movieContext.Serials.First(x => x.Id == id);
+
+        public void Update(int id, Serial newObj)
+        {
+            var tf = _movieContext.Serials.First(x => x.Id == id);
+
+            tf.Name = newObj.Name;
+            tf.Evaluation = newObj.Evaluation;
+            tf.Description = newObj.Description;
+            tf.PosterPath = newObj.PosterPath;
+
+
+            tf.Season = newObj.Season;
+            tf.Episode = newObj.Episode;
+            tf.Completed = newObj.Completed;
+
+            _movieContext.SaveChanges();
+        }
+    }
+}
